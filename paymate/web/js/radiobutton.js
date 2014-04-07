b6b799c12fa9c3af685c18c2305b3dea98c88d31
Alena Ruprecht <alena@brandwatch.com>
2014-04-07 14:12:26 +0100

    form adjustments

--- paymate/web/js/radiobutton.js
+++ paymate/web/js/radiobutton.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
   $('input[value="once"]').change(function(){
     $('.recurring').addClass('hidden');
-    $('.datelabels').text('Schedule Payment Date').css('margin-right', '64px');;
+    $('.datelabels').text('Schedule Date').css('margin-right', '0');;
   });
   
   $('input[value="recurring"]').change(function(){
     $('.recurring').removeClass('hidden');
-    $('.datelabels').text('Start Date').css('margin-right', '159px');
+    $('.datelabels').text('Start Date').css('margin-right', '31px');
   });
 });
