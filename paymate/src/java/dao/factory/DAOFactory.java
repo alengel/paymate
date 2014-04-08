7bff9920f264459da4614dc33b698bbd822be06e
Alena Ruprecht <alena@brandwatch.com>
2014-04-08 09:48:37 +0100

    refactor DaoFactory

--- paymate/src/java/dao/factory/DAOFactory.java
+++ paymate/src/java/dao/factory/DAOFactory.java
@@ -1,22 +1,26 @@
 package dao.factory;
 
 import dao.generic.AccountDao;
+import dao.generic.PaymentDao;
+import dao.generic.ScheduledPaymentDao;
 
 /**
  *
  * @author 119848
  */
+
 public abstract class DAOFactory {
 
-    public static final int JDBC = 1;
+    public static final int JPA = 1;
 
     public abstract AccountDao getAccountDAO();
-//    public abstract PaymentDAO getPaymentDAO();
+    public abstract PaymentDao getPaymentDAO();
+    public abstract ScheduledPaymentDao getScheduledPaymentDAO();
 
+    //Return correct dao factory
     public static DAOFactory getDAOFactory(int whichFactory) {
-
         switch (whichFactory) {
-            case JDBC:
+            case JPA:
                 return new JpaFactory();
             default:
                 return null;
--- paymate/src/java/dao/factory/JpaFactory.java
+++ paymate/src/java/dao/factory/JpaFactory.java
@@ -2,18 +2,32 @@ package dao.factory;
 
 import dao.generic.AccountDao;
 import dao.jpa.JpaAccountDao;
-import dao.factory.DAOFactory;
+import dao.generic.PaymentDao;
+import dao.generic.ScheduledPaymentDao;
+import dao.jpa.JpaPaymentDao;
+import dao.jpa.JpaScheduledPaymentDao;
 import javax.ejb.Stateless;
 
 /**
  *
  * @author 119848
  */
+
 @Stateless
 public class JpaFactory extends DAOFactory {
 
     @Override
     public AccountDao getAccountDAO() {
         return new JpaAccountDao();
+    }
+    
+    @Override
+    public PaymentDao getPaymentDAO() {
+        return new JpaPaymentDao();
+    }
+    
+    @Override
+    public ScheduledPaymentDao getScheduledPaymentDAO() {
+        return new JpaScheduledPaymentDao();
     }
 }
